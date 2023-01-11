e78663b35ebb9dc4b2864b20cd3ce161ad16d89d
crist138 <cristoballongares@gmail.com>
2023-01-11 17:28:35 -0600

    Codigo para dibujar contornos

--- contorno.py
+++ contorno.py
@@ -1,10 +1,13 @@
 import cv2
-img = cv2.imread('imgs/contorno.jpg')
+img = cv2.imread('imgs/monedas.jpg')
 grises = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-umbral = cv2.threshold(grises,0,255,cv2.THRESH_BINARY)
+_,umbral = cv2.threshold(grises,0,255,cv2.THRESH_BINARY)
+contorno,jerarquia = cv2.findContours(umbral, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
+cv2.drawContours(img,contorno,-1,(53, 171, 85),3)
 
 # show window bien gringo yo ajhsdkjasdh asies
-cv2.imshow('Imagen grises',grises)
+cv2.imshow('Show',img)
+#cv2.imshow('Imagen grises',grises)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
